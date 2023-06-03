d546e6add487a26bdebfb3107ddc47d2106dc088
bouguerbaahmed06@gamil.com <bouguerbaahmed06@gmail.com>
2023-06-03 21:15:44 +0200

    modifier la method edit du profil pour afficher la forme ProfilType et donner son role pour modifier le profile .

--- front/src/Controller/Candidat/ProfilController.php
+++ front/src/Controller/Candidat/ProfilController.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Controller\Candidat;
+
+use App\Entity\Profil;
 use App\Repository\ProfilRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ProfilType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response ;
 
 #[Route("/Candidat/")]
@@ -20,12 +25,23 @@ class ProfilController extends AbstractController
         ]);
     }
 
-    #[Route('modifier/{idProfil}', name: 'modifier-profil')]
-    public function edit(int $idProfil, ProfilRepository $repository) : Response
+    #[Route('profil/modifier/{id}', name: 'modifier-profil')]
+    public function edit(Profil $profil, Request $request, EntityManagerInterface $manager) : Response
     {
-        $profil = $repository->findOneBy(['id'=>$idProfil]);
 
         $form = $this->createForm(ProfilType::class , $profil);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $profil =$form->getData();
+
+            $manager->persist($profil);
+            $manager->flush();
+
+            $this->addFlash('Success' , 'Votre profile a été modifié avec success !');
+
+        }
+
         return $this->render('Candidat/modifierProfil.html.twig', [
             'form' =>$form->createView()
         ]);
