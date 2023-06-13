5857f8b36e05becb022c9040279f928270ff910f
Bouguerba Ahmed <109955655+ahmedbou2000@users.noreply.github.com>
2023-06-13 04:30:31 +0200

    Delete back directory

--- back/db.sh
+++
@@ -1,4 +0,0 @@
-php bin/console d:d:d --force
-php bin/console d:d:c
-php bin/console d:s:u -f
-php bin/console doctrine:fixtures:load
--- back/docker/docker.sh
+++
@@ -1,4 +0,0 @@
-php bin/console d:d:c
-php bin/console d:s:u -f
-#php bin/console app:create-user demo@demo.com password
-exec apache2-foreground
